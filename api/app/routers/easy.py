a2ab82bd697843b2d5ac91f382e3a0ffd3acc950
Antonin Rousseau <fanta.r55000@gmail.com>
2022-05-11 10:14:02 +0200

    implementation changes
    Codes now have an expiration date set at 10 minutes after their creation

--- api/app/routers/easy.py
+++ api/app/routers/easy.py
@@ -3,27 +3,35 @@ import random
 from fastapi import APIRouter
 import utils
 import Models
-from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import Depends, HTTPException, status
 import asyncio
+from datetime import datetime, timedelta
 router = APIRouter()
 
-easyAuth = {
-    '12345': {"sid": "rehetjhnepjohn", "token": 'tokenbidon'},
-}
-
+#dictionnaire de code et de token
+easyAuth = {}
 
 def getEasyCode():
+    """
+    Génère un code aléatoire de 5 caractères en vérifiant qu'il n'est pas déjà utilisé
+    Returns:
+        code (str): code aléatoire de 5 caractères
+    """
     run = True
     while(run):
         x = ''.join(random.choices('123456789', k=5))
         run = x in easyAuth
-    easyAuth[x] = 'tokenbidon'
+    # expires in 10 minutes
+    easyAuth[x] = {}
+    easyAuth[x]['expires'] = datetime.now() + timedelta(minutes=10)
+    easyAuth[x]['token'] = 'tokenbidon'
+    print(easyAuth)
+    print(type(easyAuth))
     return x
 
 
 @router.post("/connect")
-async def easy_login(code: Models.Easy):
+async def easy_login(code: Models.Easy, current_user: Models.User = Depends(utils.get_current_user_in_token)):
     if not code.token:
         raise HTTPException(
             status_code=status.HTTP_406_NOT_ACCEPTABLE,
@@ -34,7 +42,7 @@ async def easy_login(code: Models.Easy):
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Code appareil invalide",
         )
-    easyAuth[code.code] = code.token
+    easyAuth[code.code]['token'] = code.token
     return {'ok'}
 
 
@@ -47,19 +55,7 @@ async def get_easy_code():
 async def getToken(code: str):
     run = True
     while(run):
-        if easyAuth[code] != 'tokenbidon':
+        if easyAuth[code]['token'] != 'tokenbidon':
             run = False
         await asyncio.sleep(1)
-    return {'token': easyAuth[code]}
-
-
-@router.delete("/{code}")
-async def deleteEasy(code: str, token: str = Depends(utils.get_token)):
-    if easyAuth[code]:
-        if easyAuth[code] != token:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Token incorrect"
-            )
-    easyAuth.pop(code)
-    return {'ok'}
+    return {'token': easyAuth[code]['token']}
