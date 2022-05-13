c124d3f59048095ed9bdef4dd0ac7cbf7cf35081
Antonin Rousseau <fanta.r55000@gmail.com>
2022-05-13 14:09:18 +0200

    delete user for admin

--- api/app/routers/admin.py
+++ api/app/routers/admin.py
@@ -20,3 +20,11 @@ async def changeAdminLevel(idUser: int, adminLevel: int = Body(..., embed=True),
     user.adminLevel = adminLevel
     await user.save()
     return await Models.UserinFront.from_tortoise_orm(user)
+@router.delete('/deleteUser/{idUser}')
+async def deleteUser(idUser: int, bool: bool = Depends(utils.AdminRequired), current_user: Models.User = Depends(utils.get_current_user_in_token)):
+    user = await Models.User.get(id=idUser)
+    if user.adminLevel == current_user.adminLevel:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Vous ne pouvez pas supprimer un administrateur")
+    await user.delete()
+    return {'message': 'Utilisateur supprimé'}
