b45110f16fb640caef95439c363fc4d3dc526869
Paul LOISIL <paul.loisil9@etu.univ-lorraine.fr>
2022-05-19 14:32:19 +0200

    Removed HTTPS redirection
    This causes canceled mixed HTTP requests

--- app/src/main.js
+++ app/src/main.js
@@ -16,10 +16,11 @@ import CreateMachine from "./views/CreateMachine.vue";
 import './index.css'
 import "flowbite"
 
-// https redirection (should be done in NGINX, but for now we do it here)
-if (window.location.protocol !== 'https:' && window.location.hostname !== 'localhost') {
-    window.location.protocol = 'https:';
-}
+/** DOESNT WORK: mixed request errors **/
+// // https redirection (should be done in NGINX, but for now we do it here)
+// if (window.location.protocol !== 'https:' && window.location.hostname !== 'localhost') {
+//     window.location.protocol = 'https:';
+// }
 
 const router = createRouter({
     mode: "history",
