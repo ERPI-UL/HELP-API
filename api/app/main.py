e822635cc38677dfdd8ed75525ce48746f1964f4
Antonin Rousseau <fanta.r55000@gmail.com>
2022-05-23 14:02:36 +0200

    revert

--- api/app/main.py
+++ api/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from tortoise.contrib.fastapi import register_tortoise
 from fastapi.middleware.cors import CORSMiddleware
-from mail import simple_send,testJinja
+# from mail import simple_send,testJinja
 from routers import admin, users
 from routers import scenarios
 from routers import stats
