4208fe7fe35ff8faa99d034f2cd62dc66f8da31b
Paul LOISIL <paul.loisil9@etu.univ-lorraine.fr>
2022-06-10 20:22:33 +0200

    Removed username input verification

--- app/src/views/GenerateInvite.vue
+++ app/src/views/GenerateInvite.vue
@@ -134,11 +134,6 @@ function sendInvite() {
         credentials.familyName.focus();
         return;
     }
-    if (credentials.username.value.trim() == "") {
-        logMessage("Veuillez renseigner un nom d'utilisateur.");
-        credentials.username.focus();
-        return;
-    }
     const REGEX_EMAIL = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     if (!credentials.email.value.trim().match(REGEX_EMAIL)) {
         logMessage("Veuillez renseigner une adresse mail valide.");
