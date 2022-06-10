7dbcdde8ef77b3e43949bca1199c3e3e90f16467
Paul LOISIL <paul.loisil9@etu.univ-lorraine.fr>
2022-06-10 10:09:46 +0200

    Added error message when no last session

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -141,8 +141,22 @@ function setup() {
                     addStat("- Temps :", stringTime(session.playedSteps.map(playedStep => playedStep.time).reduce((a, b) => a+b, 0)));
                 });
             });
-        else document.getElementById("overview").classList.add("hidden");
-    });
+        else noLastSession();
+    }).catch(noLastSession);
+}
+
+// executed when no last session is available
+function noLastSession() {
+    const loadingZone = document.getElementById("loading-zone");
+    loadingZone.innerHTML = `
+        <div class="flex m-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20 text-gray-300 rotate-[-15deg]" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <!-- <img src="https://iut-charlemagne.univ-lorraine.fr/wp-content/themes/eduma/images/image-404.jpg" class="opacity-30 w-40"> -->
+            <p class="text-2xl font-extrabold text-gray-300 my-auto ml-4">Aucune session trouvée</p>
+        </div>
+    `
 }
 
 function addStat(title, data) {
