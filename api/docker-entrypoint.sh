84afdb5d696ff91dbe788a37b969cb8d3f5a2a86
toxicbloud <fanta.r55000@gmail.com>
2023-04-18 17:29:41 +0200

    LF on Linux

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -3,11 +3,11 @@ echo $DB_HOST
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
-    
+
     while ! nc -z database $SQL_DB_PORT; do
-        sleep 0.1
+      sleep 0.1
     done
-    
+
     echo "PostgreSQL started"
 fi
 
