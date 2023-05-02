899581ada1a1efa545a7ccba8e537c7b5c8a34b4
toxicbloud <fanta.r55000@gmail.com>
2023-05-02 14:35:26 +0200

    supression route data scenario

--- api/app/routers/data.py
+++ api/app/routers/data.py
@@ -7,15 +7,6 @@ from app.utils import ACTION_DATA_DIRECTORY
 router = APIRouter()
 
 
-@router.get('/scenarios/{scenario_id}/ressources/{ressource_name}')
-async def get_ressource(scenario_id: int, ressource_name: str):
-    """ Get a ressource from a step"""
-    path = f'app/data/scenarios/{scenario_id}/ressources/{ressource_name}'
-    if not await async_os_path_exists(path):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Ressource {ressource_name} not found for scenario {scenario_id}")
-    return FileResponse(path)
-
-
 @router.get('/actions/{action_id}/ressources/{ressource_name}')
 async def get_action_ressource(action_id: int, ressource_name: str):
     """ Get a ressource from a step"""
